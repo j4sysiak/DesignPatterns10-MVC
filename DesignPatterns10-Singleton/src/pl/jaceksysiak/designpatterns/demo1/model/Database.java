9363ee7f0381365bee8e0625ce2e9d8cf5599cde
j4sysiak <j4sysiak@gmail.com>
2019-02-22 06:16:59 +0100

    1

---
+++ DesignPatterns10-Singleton/src/pl/jaceksysiak/designpatterns/demo1/model/Database.java
@@ -0,0 +1,27 @@
+package pl.jaceksysiak.designpatterns.demo1.model;
+
+public class Database {
+	
+	private static Database instance = new Database();
+	
+	private Database() {
+		
+	}
+	
+	public static Database getInstance() {
+		return instance;
+	}
+	
+	/*
+	private static Database instanceOld;
+	
+	public static Database getInstanceOld() {
+		if(instanceOld == null) {
+			instanceOld = new Database();
+		}
+		
+		return instanceOld;
+	}
+	*/
+
+}
